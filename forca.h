0b0092cc8ef86f4e401fa7ded79101f975c5aeee
Matheus Ferreira <59848966+walker785@users.noreply.github.com>
2020-08-31 23:42:41 -0300

    Add files via upload

---
+++ forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA 20
+
+int enforcou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+int ganhou();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+int letraexiste(char letra);
+int chuteserrados();
